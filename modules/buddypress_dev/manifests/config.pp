d4e3a5935cdf4b3bf9f93e3b63bb7013c97b60f2
Bronson Quick <bronson@bronsonquick.com.au>
2019-08-31 14:24:00 +1000

    Create plugins directory

--- modules/buddypress_dev/manifests/config.pp
+++ modules/buddypress_dev/manifests/config.pp
@@ -3,6 +3,11 @@ class buddypress_dev::config {
 	file { '/vagrant/content/plugins/buddypress':
 		ensure  => 'link',
 		target  => '/vagrant/buddypress-develop/src/',
-		require => Class['buddypress_dev::repository'],
+		require => [ Class['buddypress_dev::repository'], File['/vagrant/content/plugins'] ],
+		force   => true,
+	}
+
+	file { '/vagrant/content/plugins':
+		ensure => 'directory',
 	}
 }
